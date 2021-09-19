5f72996db6071b753ba57cf434419e365d71539b
celop <piotr.celowski@pega.com>
2021-09-19 11:22:29 +0200

    Add insertion sorter

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] intArray = {20, 30, -5, 50, 152, -1, 87};
 
-        SelectionSorter selectionSorter = new SelectionSorter();
-        selectionSorter.sort(intArray);
+        InsertionSorter insertionSorter = new InsertionSorter();
+        insertionSorter.sort(intArray);
 
         for(int i=0; i<intArray.length; i++) {
             System.out.println(intArray[i]);
