0cb4269cef6bb5ed10a41cb90e8aa47eb2d9c951
celop <piotr.celowski@pega.com>
2021-09-19 12:24:21 +0200

    Add shell sort

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] intArray = {20, 30, -5, 50, 152, -1, 87};
 
-        InsertionSorter insertionSorter = new InsertionSorter();
-        insertionSorter.sort(intArray);
+        ShellSorter shellSorter = new ShellSorter();
+        shellSorter.sort(intArray);
 
         for(int i=0; i<intArray.length; i++) {
             System.out.println(intArray[i]);
---
+++ src/ShellSorter.java
@@ -0,0 +1,16 @@
+public class ShellSorter {
+    public void sort(int[] array) {
+        for(int gap=array.length/2; gap>0; gap/=2){
+            for(int i=gap; i<array.length; i++) {
+                int newElement = array[i];
+
+                int j=i;
+                while(j>=gap && array[j-gap]>newElement){
+                    array[j]=array[j-gap];
+                    j-=gap;
+                }
+                array[j]=newElement;
+            }
+        }
+    }
+}
